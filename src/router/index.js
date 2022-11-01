b27e64833159fb1b76672b06e02e1627fbec5f41
Vinh Nguyen <vinh466@gmail.com>
2022-11-02 06:09:10 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ const routes = [
         path: "/",
         name: "contactbook",
         component: ContactBook,
+    }, {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
     },
     {
         path: "/:pathMatch(.*)*",
